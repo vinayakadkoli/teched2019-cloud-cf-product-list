841a210c8f8d31e2d09d9c2e62b5cb755a7d64a2
vinayak Adkoli <vinayakadkoli@gmail.com>
2020-04-02 22:08:18 +0530

    initial commit

--- product-list/myapp/index.js
+++ product-list/myapp/index.js
@@ -3,8 +3,27 @@ const { getProducts, getProductsByName} = require('./lib/repository');
 const app = express();
 const port = process.env.port || 8080;
 // secure the direct call to the application
+const passport = require('passport');
+const { JWTStrategy } = require('@sap/xssec');
+const xsenv = require('@sap/xsenv');
 
-app.get('/products', getProducts);
+// XSUAA Middleware
+passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
+
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+app.get('/products', checkReadScope, getProducts);
+
+// Scope check
+function checkReadScope(req, res, next) {
+	if (req.authInfo.checkLocalScope('read')) {
+		return next();
+	} else {
+    	console.log('Missing the expected scope');
+    	res.status(403).end('Forbidden');
+	}
+}
 
 // Serve static files
 app.use('/', express.static('static/'));
